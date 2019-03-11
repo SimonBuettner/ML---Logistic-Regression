e642833014f11e4353984c5a3075cd6bc0981312
SimonBuettner <45937203+SimonBuettner@users.noreply.github.com>
2019-03-11 11:02:54 +0100

    Update

--- Linear/Linear_Regression.py
+++ Linear/Linear_Regression.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 # Read CSV-File:
 df = pd.read_csv("flat_Price.csv")
@@ -10,23 +9,32 @@ df = pd.read_csv("flat_Price.csv")
 # Returns the first 5 rows:
 print(df.head())
 
-plt.scatter(df["QM"], df["Price"])
+X= df[["QM"]].values
+Y = df[["Price"]].values
+
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, random_state = 0, test_size = 0.25)
+
+
+plt.scatter(Xtrain, Ytrain)
+plt.scatter(Xtest, Ytest, color = "red")
 plt.title("Data Points")
 plt.show()
 
 # assigns the QM as X-Axis and Price as Y-Axis
 model = LinearRegression()
-model.fit(df[["QM"]], df[["Price"]])
+model.fit(Xtrain, Ytrain)
 
 print("The prediction of the sales price of a flat with 42 sqm is: ")
 print(model.predict([[42]]))
 
+# gets the min & max value of x to adjust the line
 x_min = min(df["QM"])
 x_max = max(df["QM"])
 
-plt.scatter(df["QM"], df["Price"])
 
-predicted = model.predict([[x_min], [x_max]])
-plt.plot([x_min,x_max], predicted, color = "red")
-plt.title("Line Graph")
+# Prints a predicted-line on basis of the blue dots of trainings-data
+predicted = model.predict(Xtest)
+plt.scatter(Xtest, Ytest, color = "red")
+plt.plot(Xtest, predicted, color = "red")
+plt.title("Predicted")
 plt.show()
