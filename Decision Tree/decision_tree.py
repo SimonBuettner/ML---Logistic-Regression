728b9f9da45fd22860fe3a2a361d53273481d814
Simon Büttner <Simon@Buettner@witt-gruppe.eu>
2019-04-05 13:51:45 +0200

    Change file names

--- Decision Tree/decision_tree.py
+++ Decision Tree/decision_tree.py
@@ -1,8 +1,10 @@
-""" This code provides an overview how to create a random forest """
+"""This code gives an overview how to create a decision tree """
 
 import pandas as pd
+import graphviz
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
 from helper import plot_classifier
 
 # Read CSV-File:
@@ -18,16 +20,26 @@ Y = DF["desease"].values
 XTRAIN, XTEST, YTRAIN, YTEST = train_test_split(X, Y,
                                                 random_state=0,
                                                 test_size=0.25)
-
-# creates a random forest with a maximum node-depth of 4
-# and a minumum number of 3 examples per leaf
-MODEL = RandomForestClassifier(criterion="gini",
-                               max_depth=4,
-                               min_samples_leaf=3)
+# Creates a decesion tree model with a maximum node depth of 5
+# and with at least 2 numbers of samples per leaf
+MODEL = DecisionTreeClassifier(criterion="gini",
+                               max_depth=5,
+                               min_samples_leaf=2)
 
 MODEL = MODEL.fit(XTEST, YTEST)
 print(MODEL.score(XTEST, YTEST))
 
+# Export tree to PNG-file
+TREE = export_graphviz(MODEL, None,
+                       feature_names=["monthlySport", "monthlyHealthyFood"],
+                       class_names=["no desase", "desease"],
+                       rounded=True,
+                       filled=True)
+
+SRC = graphviz.Source(TREE, format="png")
+SRC.render("./datei", view=True)
+
+# prints the diagram
 plot_classifier(MODEL, XTRAIN, YTRAIN, proba=True,
                 xlabel="monthlySport",
                 ylabel="monthlyHealthyFood")
